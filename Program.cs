aec29e726f6a2d9aa219ff85dfa9e479f07561d2
Admin <marinsluciana53@gmail.com>
2021-02-16 16:58:35 -0200

    resoluçao uri

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uri1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro:");
+            int A = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro numero inteiro:");
+            int B = Int32.Parse(Console.ReadLine());
+            int X = (A+B);
+            Console.WriteLine("A soma de " +A+" + " +B+ " é igual a: "+X);
+        }
+    }
+}
